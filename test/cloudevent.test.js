7b9fbe70d9e56aab2a251f017ccc027331b40b48
Sandro Martini <sandro.martini@gmail.com>
2019-08-06 23:37:48 +0200

    improve code coverage

--- test/cloudevent.test.js
+++ test/cloudevent.test.js
@@ -45,28 +45,28 @@ ceMapData.set('key-2', 'value 2')
 
 /** @test {CloudEvent} */
 test('ensure CloudEvent class (and related Validator and Transformer classes) are exported by the library', (t) => {
-  t.plan(14)
+  t.plan(20)
+
+  const { CloudEvent, CloudEventValidator: V, CloudEventTransformer: T } = require('../src/') // get references via destructuring
+  t.ok(CloudEvent)
+  // optional, using some standard Node.js assert statements, as a sample
+  assert(CloudEvent !== null)
+  assert.strictEqual(typeof CloudEvent, 'function')
+  assert(new CloudEvent() instanceof CloudEvent)
+  assert.strictEqual(CloudEvent.mediaType(), 'application/cloudevents+json')
+  assert.strictEqual(CloudEvent.mediaTypeBatchFormat(), 'application/cloudevents-batch+json')
+  t.ok(V)
+  t.strictEqual(typeof CloudEvent, 'function')
+  t.strictEqual(typeof V, 'function')
+  t.strictEqual(typeof T, 'function')
+  t.ok(V.isFunction(CloudEvent))
+  t.ok(V.isFunction(V))
+  t.strictEqual(typeof CloudEvent, 'function')
+  t.strictEqual(new CloudEvent() instanceof CloudEvent, true)
+  t.strictEqual(CloudEvent.mediaType(), 'application/cloudevents+json')
+  t.strictEqual(CloudEvent.mediaTypeBatchFormat(), 'application/cloudevents-batch+json')
 
   {
-    const { CloudEvent, CloudEventValidator: V, CloudEventTransformer: T } = require('../src/') // get references via destructuring
-    t.ok(CloudEvent)
-    // optional, using some standard Node.js assert statements, as a sample
-    assert(CloudEvent !== null)
-    assert.strictEqual(typeof CloudEvent, 'function')
-    assert(new CloudEvent() instanceof CloudEvent)
-    assert.strictEqual(CloudEvent.mediaType(), 'application/cloudevents+json')
-    assert.strictEqual(CloudEvent.mediaTypeBatchFormat(), 'application/cloudevents-batch+json')
-    t.ok(V)
-    t.strictEqual(typeof CloudEvent, 'function')
-    t.strictEqual(typeof V, 'function')
-    t.strictEqual(typeof T, 'function')
-    t.ok(V.isFunction(CloudEvent))
-    t.ok(V.isFunction(V))
-    t.strictEqual(typeof CloudEvent, 'function')
-    t.strictEqual(new CloudEvent() instanceof CloudEvent, true)
-    t.strictEqual(CloudEvent.mediaType(), 'application/cloudevents+json')
-    t.strictEqual(CloudEvent.mediaTypeBatchFormat(), 'application/cloudevents-batch+json')
-
     // create an instance with only mandatory arguments (no strict mode, but doesn't matter in this case): expected success ...
     const ceMinimal = new CloudEvent('1', // id
       ceNamespace, // type
@@ -80,6 +80,27 @@ test('ensure CloudEvent class (and related Validator and Transformer classes) ar
     // check that created instances belongs to the right base class
     t.strictEqual(typeof ceMinimal, 'object')
     t.ok(V.isClass(ceMinimal, CloudEvent))
+  }
+
+  {
+    // create an instance with only mandatory arguments but null data (and strict mode): expected success ...
+    const ceMinimalStrict = new CloudEvent('1-strict', // id
+      ceNamespace, // type
+      '/', // source
+      null // data // optional, but useful the same in this sample usage
+    )
+    t.ok(ceMinimalStrict)
+
+    // check that created instances belongs to the right base class
+    t.strictEqual(typeof ceMinimalStrict, 'object')
+    t.ok(V.isClass(ceMinimalStrict, CloudEvent))
+
+    t.strictEqual(typeof ceMinimalStrict.data, 'object') // data is wrapped in an object, so even when null it's an object
+    t.ok(CloudEvent.isValidEvent(ceMinimalStrict))
+
+    // set ceMinimalStrict.data to null, to ensure validation is good the same
+    ceMinimalStrict.data = null
+    t.ok(CloudEvent.isValidEvent(ceMinimalStrict))
   }
 })
 
