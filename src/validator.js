08aa0d616d84c4ed08c5fa1cc0dcf29886a3c57a
Sandro Martini <sandro.martini@gmail.com>
2019-08-21 21:58:33 +0200

    fix bug (happening only in some cases during tests) in isDateFuture

--- src/validator.js
+++ src/validator.js
@@ -155,7 +155,7 @@ class Validator {
    * @return {boolean} true if it's a valid date in the future (or now), false otherwise
    */
   static isDateFuture (arg) {
-    return (Validator.isDateValid(arg) && arg >= Date.now())
+    return (Validator.isDateValid(arg) && arg.getTime() >= Date.now())
   }
 
   /**
